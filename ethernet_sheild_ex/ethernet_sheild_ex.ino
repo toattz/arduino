bca073bad294654ee661add9bc0724f967218ce4
Blanka Ho <blanka671107@gmail.com>
2021-05-18 16:01:11 +0800

    finish w5100 demo

--- ethernet_sheild_ex/ethernet_sheild_ex.ino
+++ ethernet_sheild_ex/ethernet_sheild_ex.ino
@@ -1,8 +1,10 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-const int b1=11,b2=3;//,b3=13;//buttin pins
-const int ledPin=13;
+const int b1=5,b2=9,b3=8;//buttin pins
+const int gLed=6,yLed=7;
+const int maxDebounce=500;
+unsigned int debounce=0;
 const char* mqttServer = "192.168.1.123"; //請確認
 byte mac[]={
   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};//指定網路卡 MAC 位址 (可以隨便設)
@@ -11,14 +13,22 @@ PubSubClient mqttClient1(client);
 void receivedCMD(char* topic, byte* payload, unsigned int noChar)
 {
   Serial.println("callback");
-  char *msg=(char)payload[0];
+  char msg=(char)payload[0];
+  //Serial.println((char)payload[0]);
   Serial.println(msg);
+  digitalWrite(yLed,HIGH);
+  delay(2000);
+  digitalWrite(yLed,LOW);
+  
 }
 void setup() {//設定初始值
-  pinMode(b1,INPUT);
-  pinMode(b2,INPUT);
-  //pinMode(b3,INPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(b1,INPUT_PULLUP);
+  pinMode(b2,INPUT_PULLUP);
+  pinMode(b3,INPUT_PULLUP);
+  pinMode(gLed, OUTPUT);
+  pinMode(yLed, OUTPUT);
+  digitalWrite(gLed,LOW);
+  digitalWrite(yLed,LOW);
 
   Serial.begin(115200);//初始化序列埠
   //啟用 Ethernet 連線，預設會以 DHCP 取得 IP 位址
@@ -33,50 +43,64 @@ void setup() {//設定初始值
 }
 void loop() 
 {
- 
-  //b1Status=digitalRead(b1);
-  //b2Status=digitalRead(b2);
-  //Serial.println(digitalRead(b1));
-  /*for(int i=2;i<=13;i++)
-  {
-    Serial.print("Port");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(digitalRead(b2));
-    
-  }*/
-  /*Serial.print("Low:");
-  Serial.println(LOW);
-  delay(4000);*/
-  if(digitalRead(b1)==HIGH)
+  if(digitalRead(b1)==LOW)
   { 
-    digitalWrite(ledPin,HIGH);
-    while(digitalRead(b1)==HIGH){
-         /*Serial.print("Port");
-    Serial.print(11);
-    Serial.print(": ");
-    Serial.println(digitalRead(b2));*/
+    if(debounce>maxDebounce)
+    {   
+      digitalWrite(gLed,HIGH);
+      while(digitalRead(b1)==LOW){
+  
+      }
+      debounce=0;
+      digitalWrite(gLed,LOW);
+      Serial.println("B1");
+      mqttClient1.publish("w5100","B1");
     }
-    digitalWrite(ledPin,LOW);
-    Serial.println("B1");
-    mqttClient1.publish("w5100","B1");
+    else
+      debounce++;
   }
-  if(digitalRead(b2)==HIGH)
+  if(digitalRead(b2)==LOW)
   {
-    while(digitalRead(b2)==HIGH){
-      digitalWrite(ledPin,HIGH);
-      delay(500);
-      digitalWrite(ledPin,LOW);
-      delay(500);
-    } 
-    Serial.println("B2");
-    mqttClient1.publish("w5100","B2");
+    if(debounce>maxDebounce)
+    {
+      while(digitalRead(b2)==LOW)
+      {
+        digitalWrite(gLed,HIGH);
+        delay(500);
+        digitalWrite(gLed,LOW);
+        delay(500);
+      } 
+      debounce=0;
+      digitalWrite(gLed,LOW);
+      Serial.println("B2");
+      mqttClient1.publish("w5100","B2");
+    }
+    else
+      debounce++;
   }
-  /*if(digitalRead(b3)==LOW)
+  if(digitalRead(b3)==LOW)
   {
-   while(digitalRead(b3)==LOW){} 
-   Serial.println("B3");
-   }*/
+    if(debounce>maxDebounce)
+    {
+      while(digitalRead(b3)==LOW)
+      {
+        digitalWrite(gLed,HIGH);
+        delay(200);
+        digitalWrite(gLed,LOW);
+        delay(200);
+      }
+      debounce=0;
+      digitalWrite(gLed,LOW);
+      Serial.println("B3");
+      mqttClient1.publish("w5100","B3");
+    }
+    else
+      debounce++;
+  }
+  /*digitalWrite(gLed,LOW);
+  delay(2000);
+  digitalWrite(gLed,HIGH);
+  delay(2000);*/
   while(!mqttClient1.connected()) {
     mqttClient1.connect("Arduino");
     mqttClient1.subscribe("test");
--- ex_2buttons/ex_2buttons.ino
+++ ex_2buttons/ex_2buttons.ino
@@ -1,12 +1,15 @@
 
-const int b1=11,b2=3;//,b3=13;//buttin pins
-const int ledPin=13;
+const int b1=5,b2=9;//,b3=13;//buttin pins
+const int ledPin=6;
+const int maxDebounce=500;
+unsigned int debounce=0;
 
 void setup() {//設定初始值
-  pinMode(b1,INPUT);
-  pinMode(b2,INPUT);
+  pinMode(b1,INPUT_PULLUP);
+  pinMode(b2,INPUT_PULLUP);
   //pinMode(b3,INPUT);
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin,LOW);
 
   Serial.begin(115200);//初始化序列埠
   //啟用 Ethernet 連線，預設會以 DHCP 取得 IP 位址
@@ -16,32 +19,40 @@ void loop()
 {
   if(digitalRead(b1)==LOW)
   { 
-    digitalWrite(ledPin,HIGH);
-    while(digitalRead(b1)==HIGH){
-         /*Serial.print("Port");
-    Serial.print(11);
-    Serial.print(": ");
-    Serial.println(digitalRead(b2));*/
+    if(debounce>maxDebounce)
+    {
+      digitalWrite(ledPin,HIGH);
+      while(digitalRead(b1)==LOW){
+     
+      }
+      debounce=0;
+      digitalWrite(ledPin,LOW);
+      Serial.println("B1");
     }
-    digitalWrite(ledPin,LOW);
-    Serial.println("B1");
-    
+    else
+      debounce++; 
   }
   if(digitalRead(b2)==LOW)
   {
-    while(digitalRead(b2)==HIGH){
-      digitalWrite(ledPin,HIGH);
-      delay(500);
-      digitalWrite(ledPin,LOW);
-      delay(500);
+    if(debounce>maxDebounce)
+    {
+      while(digitalRead(b2)==LOW){
+        digitalWrite(ledPin,HIGH);
+        delay(500);
+        digitalWrite(ledPin,LOW);
+        delay(500);
     } 
-    Serial.println("B2");
-    
+      debounce=0;
+      digitalWrite(ledPin,LOW);
+      Serial.println("B3");   
+    }
+    else
+      debounce++;
   }
   /*if(digitalRead(b3)==LOW)
   {
    while(digitalRead(b3)==LOW){} 
    Serial.println("B3");
    }*/
-  
-} //無限迴圈
+ 
+}
